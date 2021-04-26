4e7f2d693621aaa3cb69fee5605c444036ded0d1
Carlos Sanchez <carlos.david.sanchez.moreno@gmail.com>
2021-04-25 20:03:11 -0600

    Changes within user application

--- pem/users/models/user.py
+++ pem/users/models/user.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Django
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 # Python & Third Parties
 # - - -
@@ -8,7 +8,7 @@ from django.db import models
 from pem.utils.models import CommonModel
 
 
-class User(CommonModel, AbstractBaseUser):
+class User(CommonModel, AbstractUser):
     USERNAME_FIELD = "username"
 
     username = models.CharField(max_length=16, unique=True)
@@ -20,18 +20,11 @@ class User(CommonModel, AbstractBaseUser):
     is_superuser = models.BooleanField(default=False)
     objects = UserManager()
 
-    def has_perm(self, permission_name : str):
-        if self.is_superuser or self.is_staff:
-            return True
-        return False
-
-    def has_module_perms(self, module : str):
-        if self.is_superuser or self.is_staff:
-            return True
-        return False
-
     def __unicode__(self):
         return u"{} {}".format(self.first_name, self.last_name)
 
     def __str__(self):
         return u"{} {}".format(self.first_name, self.last_name)
+
+    class Meta:
+        db_table = "users"
